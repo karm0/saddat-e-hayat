516e9d73c660a5d9901dfd03e2d4750589c1d32d
karm <75268104+karm0@users.noreply.github.com>
2023-03-17 06:22:17 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,4 +2,19 @@ let btnList = document.getElementById('btnList');
 
 btnList.addEventListener('click',()=>{
   navList.classList.toggle('active')
-  })
+})
+
+let donate = document.querySelectorAll('.click-donate');
+let pay = document.querySelector('.pay')
+
+
+
+donate.forEach(function(ele) {
+  ele.onclick = () => {
+    pay.classList.add('active2');
+  }
+})
+
+closeDiv.addEventListener('click',()=>{
+  pay.classList.remove('active2')
+})
