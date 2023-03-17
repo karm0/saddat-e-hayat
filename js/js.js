bad800050c4799488188d791b579b3bb3a5600c9
karm <75268104+karm0@users.noreply.github.com>
2023-03-17 19:37:18 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -18,3 +18,38 @@ donate.forEach(function(ele) {
 closeDiv.addEventListener('click',()=>{
   pay.classList.remove('active2')
 })
+
+
+
+
+
+
+
+const slider = document.querySelector('.slider');
+const sliderContainer = slider.querySelector('.slider-container');
+const prevBtn = slider.querySelector('.prev-btn');
+const nextBtn = slider.querySelector('.next-btn');
+const cards = slider.querySelectorAll('.card');
+const cardWidth = cards[0].offsetWidth;
+let currentIndex = 0;
+
+function slideLeft() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = cards.length - 1;
+  }
+  sliderContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+}
+
+function slideRight() {
+  currentIndex++;
+  if (currentIndex > cards.length - 1) {
+    currentIndex = 0;
+  }
+  sliderContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+}
+
+prevBtn.addEventListener('click', slideLeft);
+nextBtn.addEventListener('click', slideRight);
+
+setInterval(slideRight, 2000);
